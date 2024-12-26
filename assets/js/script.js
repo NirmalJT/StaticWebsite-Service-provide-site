1c4ab13b2c896fb06a49bd90f769c0e75f5bbd84
NirmalJT <nirmaljyotithakuria@gmail.com>
2024-12-26 21:56:42 +0530

    add terms and condition and it complete responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,19 +42,39 @@ function handleHide(index) {
   }
 }
 
-
-
 //typed js
 
-// var options = {
-//   strings: [
-//     "MIGHTYTECH CONSULTANCY SERVICE",
-//   ],
-//   typeSpeed: 70, // Speed of typing in milliseconds
-//   backSpeed: 60, // Speed of backspacing
-//   backDelay: 1000, // Delay before starting to backspace
-//   startDelay: 500, // Delay before typing starts
-//   loop: true, // Whether to loop the animation
-// };
+var options = {
+  strings: ["MIGHTYTECH CONSULTANCY SERVICE"],
+  typeSpeed: 70, // Speed of typing in milliseconds
+  backSpeed: 60, // Speed of backspacing
+  backDelay: 1000, // Delay before starting to backspace
+  startDelay: 500, // Delay before typing starts
+  loop: true, // Whether to loop the animation
+};
 
-// var typed = new Typed("#typed-output", options);
+var typed = new Typed("#typed-output", options);
+
+//Terms & condition
+
+const TermsCon_BTN = document.getElementById("Terms&ConBTN");
+const termsAndConditions = document.getElementById("terms-and-conditions");
+const closeTermsBTN = document.getElementById("closeTermsBTN");
+
+// Show modal
+TermsCon_BTN.addEventListener("click", () => {
+  termsAndConditions.classList.remove("hidden");
+  termsAndConditions.classList.add("flex", "opacity-0");
+  setTimeout(() => {
+    termsAndConditions.classList.remove("opacity-0");
+  }, 50);
+});
+
+// Hide modal
+closeTermsBTN.addEventListener("click", () => {
+  termsAndConditions.classList.add("opacity-0");
+  setTimeout(() => {
+    termsAndConditions.classList.add("hidden");
+    termsAndConditions.classList.remove("flex");
+  }, 300);
+});
